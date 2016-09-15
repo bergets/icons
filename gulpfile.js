c6168b55d5f440b18e14033299cc5203cdd00a0f
henrikgram <henrik@lovables.se>
2016-09-15 18:12:21 +0200

    finished builds, added lots of icons

--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,36 @@ var gulp = require('gulp'),
   plumber = require('gulp-plumber'),
   inject = require('gulp-inject'),
   runSequence = require('run-sequence'),
+  svgmin = require('gulp-svgmin'),
+  del = require('del'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  cssnano = require('cssnano'),
+  customProperties = require("postcss-custom-properties"),
+  nested = require("postcss-nested"),
+  easyImport = require("postcss-easy-import"),
   browserSync = require('browser-sync').create();
 
-var siteDir = './dist/'
+var siteDir = './'
 
 var paths = {
   html:{
     index: './src/index.html',
-    dist: './dist/'
+    out: './'
   },
   svg: {
     app: './src/icn/**/*.svg',
-    dist: './dist/svg',
+    dist: './dist/',
+    icons: 'icons/',
     sprite: 'sprite.svg'
+  },
+  css: {
+    app: './src/style.css',
+    out: './',
   },
 }
 
 var svgConfig                  = {
-  shape: {
-    dest: "intermediate-svg",
-  },
   mode                    : {
       dest: "",
       inline              : true,
@@ -42,7 +52,7 @@ gulp.task('build:svg:symbol', function(){
 
 gulp.task('build:svg:inject', function(){
   var svgs = gulp
-    .src('./dist/svg/sprite.svg')
+    .src(paths.svg.dist + paths.svg.sprite)
 
   function fileContents (filePath, file) {
         return file.contents.toString();
@@ -50,11 +60,50 @@ gulp.task('build:svg:inject', function(){
 
   return gulp.src(paths.html.index)
     .pipe(inject(svgs, { transform: fileContents }))
-    .pipe(gulp.dest(paths.html.dist));
+    .pipe(gulp.dest(paths.html.out));
 });
 
+gulp.task('build:svg:min', function () {
+    return gulp.src(paths.svg.app)
+        .pipe(svgmin())
+        .pipe(gulp.dest(paths.svg.dist + paths.svg.icons));
+});
+
+gulp.task('build:styles', function() {
+  var processors = [
+    autoprefixer({browsers: ['last 1 version']}),
+    easyImport,
+    customProperties,
+    nested,
+    cssnano
+];
+  return gulp.src(paths.css.app)
+    .pipe(postcss(processors))
+    .pipe(gulp.dest(paths.css.out))
+});
+
+gulp.task('clean', function () {
+  return del([
+    'dist/**/*',
+  ]);
+});
+
+
+// Task for creating svg symbols and svg symbols minified, smallest task when only wanting icons
+gulp.task('build', function(cb) {
+  runSequence(['build:svg:symbol', 'build:svg:min'],
+              cb);
+});
+
+// Task to delete content in dist folder and creating everything from scratch, remove old files
+gulp.task('production', function(cb) {
+  runSequence('clean',['build:svg:symbol', 'build:svg:min', 'build:styles'], 'build:svg:inject',
+              cb);
+});
+
+// Task for creating svg symbols and styles and index.html, used with serve
 gulp.task('build:svg', function(cb) {
-  runSequence('build:svg:symbol', 'build:svg:inject',
+  runSequence(['build:svg:symbol', 'build:styles'], 'build:svg:inject',
               cb);
 });
 
@@ -68,12 +117,13 @@ gulp.task('build:svg:inject:watch', ['build:svg:inject'], function(cb) {
   cb();
 });
 
-gulp.task('build', function(cb) {
-  runSequence(['build:svg', 'build:svg:inject'],
-              cb);
+gulp.task('build:styles:watch', ['build:styles'], function(cb) {
+  browserSync.reload();
+  cb();
 });
 
-gulp.task('serve', ['build'], function() {
+
+gulp.task('serve', ['build:svg'], function() {
 
   browserSync.init({
     server: siteDir,
@@ -85,7 +135,10 @@ gulp.task('serve', ['build'], function() {
   // Watch SVG
   gulp.watch(['src/icn/**.*.svg', 'src/icn/*.svg'], ['build:svg:watch']);
 
-  // Watch Inject SVG
+  // Watch index.html html and inject SVG
   gulp.watch(['src/index.html', 'src/icn/**.*.svg'], ['build:svg:inject:watch']);
+
+  // Watch css html and inject SVG
+  gulp.watch(['src/*.css', 'src/icn/**.*.css'], ['build:styles:watch']);
 
 });
